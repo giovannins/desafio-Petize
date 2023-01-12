a44936dcce9ab3334326e3008fdc0fe0bbb253a4
Giovanni Neves <giovanni_neves@tutanota.com>
2023-01-12 17:25:49 -0300

    Função de Fetch para dados da API

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,11 +9,35 @@ import { ActivatedRoute } from '@angular/router';
 
 
 export class ProfileComponent {
-  user: string|null;
+  isLoading: boolean = true
+  user: string | null;
   repoData: any;
+  userData: any;
 
   constructor(private route: ActivatedRoute) {
     this.user = this.route.snapshot.queryParamMap.get('user')
+
+    // Fetch nas informações do User
+    fetch(`https://api.github.com/users/${this.user}`)
+    .then(response => response.json())
+    .then(data => {
+      this.userData = data
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    // Fetch nas Repo do user
+    fetch(`https://api.github.com/users/${this.user}/repos`)
+      .then(response => response.json())
+      .then(data => {
+        this.repoData = data
+
+        this.isLoading = false;
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
 
   }
