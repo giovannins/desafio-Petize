64ec4907e9423b55863e471f0c4b9fa644b69b2f
Giovanni Neves <giovanni_neves@tutanota.com>
2023-01-13 19:11:01 -0300

    Finalizando teste com overflow

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 
 
 export class ProfileComponent {
-  isLoading: boolean = true
+  isLoading: boolean = true;
   user: string | null;
   repoData: any;
   userData: any;
@@ -19,26 +19,33 @@ export class ProfileComponent {
 
     // Fetch nas informações do User
     fetch(`https://api.github.com/users/${this.user}`)
-    .then(response => response.json())
-    .then(data => {
-      this.userData = data
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        this.userData = data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
     // Fetch nas Repo do user
     fetch(`https://api.github.com/users/${this.user}/repos`)
       .then(response => response.json())
       .then(data => {
-        this.repoData = data
+        this.repoData = data.reverse();
 
         this.isLoading = false;
       })
       .catch(error => {
         console.log(error);
       });
+  }
 
-
+  atualizarTempo(dateInput: string) {
+    const dateString = dateInput.substring(0, 10);
+    const date = new Date(dateString);
+    const today = new Date();
+    const diff = today.getTime() - date.getTime();
+    const diffInDays = diff / (1000 * 60 * 60 * 24);
+    return Math.floor(diffInDays);
   }
 }
